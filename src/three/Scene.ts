46cf24ebe016ae6581e031bb5bb33e415c1c0af1
Dipokal HHJ <hhj@devent.kr>
2024-07-09 19:51:21 +0900

    feat: update class

--- src/three/Scene.ts
+++ src/three/Scene.ts
@@ -1,40 +1,35 @@
 import * as THREE from "three";
 
 class Scene {
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+  camera: any;
+  mesh: any;
   constructor() {
-    this.init();
-  }
-
-  init() {
     const width = window.innerWidth,
       height = window.innerHeight;
 
-    // init
+    this.camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    this.camera.position.z = 1;
 
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    camera.position.z = 1;
-
-    const scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
     const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     const material = new THREE.MeshNormalMaterial();
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(this.mesh);
 
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(width, height);
+    this.renderer.setAnimationLoop(this.animate.bind(this));
+    document.body.appendChild(this.renderer.domElement);
+  }
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(width, height);
-    renderer.setAnimationLoop(animate);
-    document.body.appendChild(renderer.domElement);
+  animate(time: number) {
+    this.mesh.rotation.x = time / 2000;
+    this.mesh.rotation.y = time / 1000;
 
-    // animation
-
-    function animate(time: number) {
-      mesh.rotation.x = time / 2000;
-      mesh.rotation.y = time / 1000;
-
-      renderer.render(scene, camera);
-    }
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
